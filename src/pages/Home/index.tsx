b710b6defdee9ac67f8ac4bb022a92fd0215c4d9
WXLWEB <luluwuweb@gmail.com>
2021-08-30 12:05:39 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -366,8 +366,9 @@ const Home: React.FC = () =>  {
                             <a href="/">Home</a>
                             <a href="#Experience">Product</a>
                             <a target="_blank" rel="noopener" href="/RAI-Finance.pdf">Whitepaper</a>
-                            <a href="https://rai-finance.notion.site/62225248bc054e1eb7ff14f8def9b17e?v=3ea69551a4a943169d4d04a8bcc8f838">Roadmap</a>
-                            <Button type="primary"><a href="https://betapro.rai.finance">Launch Beta</a></Button>
+                            <a target="_blank" href="https://rai-finance.notion.site/62225248bc054e1eb7ff14f8def9b17e?v=3ea69551a4a943169d4d04a8bcc8f838">Roadmap</a>
+                            <a target="_blank" href="https://rai-finance.gitbook.io/rai-finance/">Wiki</a>
+                            <Button type="primary"><a target="_blank" href="https://betapro.rai.finance">Launch Beta</a></Button>
                         </nav>
                     </CSSTransition>
                     <button onClick={toggleNav} className="Burger">
